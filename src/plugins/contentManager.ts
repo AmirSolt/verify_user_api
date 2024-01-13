8f16f63c9a6e770324c750f3d7a817267b97f086
Amir <soltamir194@gmail.com>
2024-01-13 01:43:50 -0500

    upd

--- src/plugins/contentManager.ts
+++ src/plugins/contentManager.ts
@@ -68,9 +68,9 @@ const emailHTMLTemplate = (verify_code:string|null, verify_link:string|null, app
    const verifyCodeHTML = `<td style="padding:30px;font-size:50px;letter-spacing:0.3em;font-family:Avenir,Helvetica,Arial,sans-serif;color:#252525;text-align:center">${verify_code}</td>`
    
    const verifyLinkHTML = `
-   <td style="padding:30px;font-size:50px;letter-spacing:0.3em;font-family:Avenir,Helvetica,Arial,sans-serif;color:#252525;text-align:center">
+   <td style="font-family:Avenir,Helvetica,Arial,sans-serif;text-align:center">
  
-   <a href="${verify_link}" target="_blank" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
+   <a href="${verify_link}" target="_blank" style="background-color: #17a607; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
       Verify Email
    </a>
    </td>
@@ -80,44 +80,44 @@ const emailHTMLTemplate = (verify_code:string|null, verify_link:string|null, app
 
 
    return `
-<div class="">
-    <div class="aHl"></div>
-    <div id=":pa" tabindex="-1"></div>
-    <div id=":p0" class="ii gt" jslog="20277; u014N:xr6bB; 1:WyIjdGhyZWFkLWY6MTc4NjQ3MzM5NTQ2Nzk1Mjc1MCJd; 4:WyIjbXNnLWY6MTc4NjQ3MzM5NTQ2Nzk1Mjc1MCJd">
-       <div id=":oz" class="a3s aiL ">
-          <u></u>
-          <div style="margin:0;padding:0">
-             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse">
-                <tbody>
-                   <tr>
-                      <td style="padding:20px;font-size:16px;font-family:Avenir,Helvetica,Arial,sans-serif;color:#252525">Hi,</td>
-                   </tr>
-                   <tr>
-                      <td style="padding:30px;font-size:16px;font-family:Avenir,Helvetica,Arial,sans-serif;line-height:19px;color:#252525;text-align:center">Your ${app_name} email verification: </td>
-                   </tr>
-                   <tr>
-                     ${verifyHTML}
-                   </tr>
-                   <tr>
-                      <td style="padding:30px;font-size:16px;font-family:Avenir,Helvetica,Arial,sans-serif;line-height:19px;color:#252525;text-align:center">
-                         Don’t share the code with anyone.
-                         <br><br>
-                         If you did not request verification, someone may be trying to access your account. You can change your password to secure your account.
-                      </td>
-                   </tr>
-                </tbody>
-             </table>
-             <div class="yj6qo"></div>
-             <div class="adL">
-             </div>
-          </div>
-          <div class="adL">
-          </div>
-       </div>
-    </div>
-    <div class="hi"></div>
-    <div class="WhmR8e" data-hash="0"></div>
- </div>
+   <div class="">
+   <div class="aHl"></div>
+   <div id=":pa" tabindex="-1"></div>
+   <div id=":p0" class="ii gt" jslog="20277; u014N:xr6bB; 1:WyIjdGhyZWFkLWY6MTc4NjQ3MzM5NTQ2Nzk1Mjc1MCJd; 4:WyIjbXNnLWY6MTc4NjQ3MzM5NTQ2Nzk1Mjc1MCJd">
+      <div id=":oz" class="a3s aiL ">
+         <u></u>
+         <div style="margin:0;padding:0">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse">
+               <tbody>
+                  <tr>
+                     <td style="padding:20px;font-size:16px;font-family:Avenir,Helvetica,Arial,sans-serif;color:#252525">Hi,</td>
+                  </tr>
+                  <tr>
+                     <td style="padding:30px;font-size:16px;font-family:Avenir,Helvetica,Arial,sans-serif;line-height:19px;color:#252525;text-align:center">Your <b>${app_name}</b> email verification: </td>
+                  </tr>
+                  <tr>
+                    ${verifyHTML}
+                  </tr>
+                  <tr>
+                     <td style="padding:30px;font-size:16px;font-family:Avenir,Helvetica,Arial,sans-serif;line-height:19px;color:#252525;text-align:center">
+                        For security purposes, don not share the content of this email with anyone.
+                        <br><br>
+                        If you did not request verification, someone may be trying to access your account. You can change your password to secure your account.
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+            <div class="yj6qo"></div>
+            <div class="adL">
+            </div>
+         </div>
+         <div class="adL">
+         </div>
+      </div>
+   </div>
+   <div class="hi"></div>
+   <div class="WhmR8e" data-hash="0"></div>
+</div>
 `
 
 }
